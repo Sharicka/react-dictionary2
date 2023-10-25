b6565bfee2d02a591bd7b7ebd6ccec046ae853d4
Sharicka <sharickassc@gmail.com>
2023-10-25 13:15:09 -0500

    Created a few Components for Dictionary App, Meaning and Results

---
+++ src/Dictionary.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+import Results from "./Results";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = "3f6be1c407b0d9d1933561808db358ba";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  }
+  axios.get(apiUrl).then(handleResponse);
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
+}
